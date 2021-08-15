9e3556e2fd7168bde2891304f3b105d30d63515c
anurosan <56799479+anurosan@users.noreply.github.com>
2021-08-15 18:13:06 -0400

    Making it so that only one data source is instantiated at a time.
    In the past, any attempt to use this class would result in all the data sources being instantiated. Now, only the one the user requested or jhu will be used by default.

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -4,12 +4,13 @@ from ..services.location.jhu import JhuLocationService
 from ..services.location.nyt import NYTLocationService
 
 # Mapping of services to data-sources.
+"""
 DATA_SOURCES = {
     "jhu": JhuLocationService(),
     "csbs": CSBSLocationService(),
     "nyt": NYTLocationService(),
 }
-
+"""
 
 def data_source(source):
     """
@@ -18,4 +19,11 @@ def data_source(source):
     :returns: The service.
     :rtype: LocationService
     """
+    if source = "csbs":
+        return CSBSLocationService()
+    elif source = "nyt":
+        return NYTLocation Service()
+    else:
+        return JhuLocationService()
+    
     return DATA_SOURCES.get(source.lower())
